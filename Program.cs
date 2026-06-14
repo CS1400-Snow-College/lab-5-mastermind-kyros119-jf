c84f3b72cd217624319c43c36ab5d7925ed6ac86
Jeremy <jeremy.frank@students.snow.edu>
2026-06-13 22:22:43 -0600

    add guess counter

--- Program.cs
+++ Program.cs
@@ -16,19 +16,26 @@ string secret = "fabc";
 // This is where the user guess will be stored
 string guess ="";
 
+// this will count how many guesses
+int guessCount = 0;
+
 //Keep asking while the guess is not equal to the secret
 do
 {
-    
-    //Ask the user for a guess
+    //this add a guess count to the guess. 
+    guessCount = guessCount + 1;
+
+
+    //Ask the user for a guess and display the guess count.
+    Console.WriteLine("Guess #" + guessCount + ":");
     Console.Write("Please gues 4 letters with no repeats:");
 
     // The user's guess
     guess = Console.ReadLine () ??"";
 
-
+// the loop will still keep going until the secret letter combination is entered.
 }while (guess != secret);
 
 // This only runs after the user guesses the secret.
 Console.WriteLine();
-Console.WriteLine("You did it! You guessed my secret.");
+Console.WriteLine("You did it! You guessed my secret: "+ secret + " after " + guessCount + " guesses! ");
