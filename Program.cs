122df44d236df60733b9a9867b490c994cee28fe
Jeremy <jeremy.frank@students.snow.edu>
2026-06-13 22:00:40 -0600

    I added a description how the game is played

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
-﻿// Jeremy Frank 6/13/2026 Lab 5 - Mastermind
+﻿// Jeremy Frank
+// 06/13/2026
+// Lab 5 - Mastermind
 
-Console.WriteLine("Hello, World!");
+// Display a greeting and explain the game to the user.
+Console.WriteLine("Welcome to Mastermind!");
+Console.WriteLine();
+Console.WriteLine("I have chosen 4 letters between 'a' and 'g'.");
+Console.WriteLine("Your job is to guess the letters in the correct order.");
+Console.WriteLine("The secret has no repeated letters.");
+Console.WriteLine();
